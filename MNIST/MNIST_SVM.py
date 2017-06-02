45e69af1fa83165bebe813bde0442bee9d9a5d9f
lankuohsing <lankuohsing@163.com>
2017-06-02 14:22:31 +0800

    no commits

--- MNIST/MNIST_SVM.py
+++ MNIST/MNIST_SVM.py
@@ -16,9 +16,16 @@ labels = labeled_images.iloc[0:5000,:1]
 train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.8, random_state=0)
 
 
-i=1
+'''
+#viewing an image
+i=1     #viewing the i-th image
 img=train_images.iloc[i].as_matrix()
 img=img.reshape((28,28))
 plt.imshow(img,cmap='gray')
 plt.title(train_labels.iloc[i,0])
 plt.hist(train_images.iloc[i])
+'''
+clf = svm.SVC()
+clf.fit(train_images, train_labels.values.ravel())
+accuracy=clf.score(test_images,test_labels)
+print(accuracy)
