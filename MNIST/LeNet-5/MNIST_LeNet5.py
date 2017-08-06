1f551e2243fecdf8b27986842c9c6639aa5d0112
lankuohsing <lankuohsing@163.com>
2017-08-06 21:06:12 +0800

    create MNIST_LeNet5

---
+++ MNIST/LeNet-5/MNIST_LeNet5.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  6 21:04:52 2017
+
+@author: lankuohsing
+"""
+
+# coding: utf-8
+
+# In[1]:
+
+#packages
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from numpy import random as nr
