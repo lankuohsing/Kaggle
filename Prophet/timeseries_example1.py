f65b50aa42d04dd3bdb9155bb23907853efae88f
languoxing <languoxing@jd.com>
2017-09-24 17:06:40 +0800

    no

--- Prophet/timeseries_example1.py
+++ Prophet/timeseries_example1.py
@@ -9,6 +9,7 @@ Created on Sat Sep 23 18:11:43 2017
 import pandas as pd
 import numpy as np
 from fbprophet import Prophet
+import matplotlib.pyplot  as plt
 # In[]
 # Python
 df = pd.read_csv('./example_wp_peyton_manning.csv')#读取数据
@@ -26,17 +27,24 @@ future.tail()
 # In[]
 
 # In[]
+# Python
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
 # In[]
+# Python
+m.plot(forecast);
 
 # In[]
+# Python
+m.plot_components(forecast);
 
 # In[]
-
+plt.plot(forecast['ds'],forecast['yhat'])
 # In[]
-
+plt.plot(df['y'])
 # In[]
-
+plt.plot(forecast['yhat'])
 # In[]
 
 # In[]
